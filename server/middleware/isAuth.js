7f256356b1c6f7232220e4625220d529598ec820
Gobind <gobindjeet78@gmail.com>
2024-01-21 12:12:30 +0530

    first commit

---
+++ server/middleware/isAuth.js
@@ -0,0 +1,20 @@
+
+const jwt = require('jsonwebtoken')
+
+// Middleware to authenticate token
+exports.authenticateToken = (req,res, next) => {
+    const token = req.cookies.token;
+
+    if(token == null){
+        req.user == null;
+        next()
+    }else{
+        const decodedToken = jwt.verify(token, process.env.SECRET_JWT)
+        if(decodedToken){
+            req.user = decodedToken._id;
+        }else{
+            req.user = null
+        }
+        next()
+    }
+}
